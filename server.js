ab884c13ef7d3549a7f0515156ee0eba338cd558
Joshua Dyck <josh.dyck@inlightmedia.ca>
2015-11-20 10:45:55 -0500

    add POST for todos

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id: 1,
-	description: 'Meet Mom for lunch',
-	completed: false
-},
-{
-	id: 2,
-	description: 'Go to the market',
-	completed: false 
-},
-{
-    id: 3,
-	description: 'Go to the mailbox',
-	completed: true
-}];
+var todos = [];
+var toDoNextId = 1;
+
+// Middleware
+app.use(bodyParser.json()); //takes data posted and makes it JSON
 
 //GET /todos get request http /todos
-//GET /todos/:id
-
 app.get('/todos', function (req, res){
 	res.json(todos);
 });
 
+//GET /todos/:id
 app.get('/todos/:id', function (req, res){
    var todoId = Number(req.params.id);      
    var matchFound;
@@ -40,15 +30,29 @@ app.get('/todos/:id', function (req, res){
    		res.json(matchFound);
    	} else {
    		res.status(404).send();
-   }
-   //res.json(todos[todo]);
-   //res.send('Asking for todo wi id: ' + req.params.id);
+   }  
 });
 
 app.get('/', function (req, res){
 	res.send('Todo API Root');
 });
 
+//POST REQUSET /todos - can take data
+// needs body-parseer module to send JSON data with post
+app.post('/todos', function (req, res) {
+	var body = req.body;
+	todos.push(body);	
+
+	body.id = toDoNextId; 
+	toDoNextId += 1;
+	//toDoNextId +=1;
+	console.log(toDoNextId + 'is the next ID');
+	console.log('description: ' + body.description);
+	res.json(body);
+});
+
 app.listen(PORT, function () {
 	console.log('Express listening on port' + PORT + '!');
-})
+});
+
+
