c9e0a93c78b340f62db06c1bb469aff341d95e7a
Joshua Dyck <josh.dyck@inlightmedia.ca>
2015-12-07 20:47:40 -0500

    added user.js to work with POST /users

---
+++ models/user.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes){
+  return  sequelize.define('user', {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true, //makes sure there is no other users in the sdatabse with the same name
+      validate: {
+        isEmail: true //does teh email validation for us
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isEmpty: false,
+      validate: {
+        len: [7, 100],
+        notEmpty: true
+
+      }
+    }
+  });
+};
