9143b543f0bfd58fb3144e8fa15d5c6942e78357
Joshua Dyck <josh.dyck@inlightmedia.ca>
2015-12-10 15:06:07 -0500

    Add Auth Token Generatoin to POST /users/login

--- models/user.js
+++ models/user.js
@@ -1,76 +1,95 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
-
-module.exports = function(sequelize, DataTypes){
-  var user = sequelize.define('user', {
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true, //makes sure there is no other users in the sdatabse with the same name
-      validate: {
-        isEmail: true //does teh email validation for us
-      }
-    },
-    salt: {
-      type: DataTypes.STRING
-    },
-    password_hash: {
-      type: DataTypes.STRING
-    },
-    password: {
-      type: DataTypes.VIRTUAL, //virutal passwords are not storse on database
-      allowNull: false,
-      isEmpty: false,
-      validate: {
-        len: [7, 100],
-        notEmpty: true
-      },
-      set: function(value){
-        var salt = bcrypt.genSaltSync(10); //10 is the number of chars in the salt or "randomization code"
-        var hashedPassword = bcrypt.hashSync(value, salt); //value is te password
-        this.setDataValue('password', value);
-        this.setDataValue('salt', salt);
-        this.setDataValue('password_hash', hashedPassword);
-      }
-    }
-  }, {
-    hooks: {
-      beforeValidate: function(user, options){
-        if (typeof user.email === 'string'){
-          user.email = user.email.toLowerCase().trim();
+module.exports = function(sequelize, DataTypes) {
+    var user = sequelize.define('user', {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true, //makes sure there is no other users in the sdatabse with the same name
+            validate: {
+                isEmail: true //does teh email validation for us
+            }
+        },
+        salt: {
+            type: DataTypes.STRING
+        },
+        password_hash: {
+            type: DataTypes.STRING
+        },
+        password: {
+            type: DataTypes.VIRTUAL, //virutal passwords are not storse on database
+            allowNull: false,
+            isEmpty: false,
+            validate: {
+                len: [7, 100],
+                notEmpty: true
+            },
+            set: function(value) {
+                var salt = bcrypt.genSaltSync(10); //10 is the number of chars in the salt or "randomization code"
+                var hashedPassword = bcrypt.hashSync(value, salt); //value is te password
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
+            }
         }
-      }
-    },
-    classMethods: {
-      authenticate: function (body) {
-        return new Promise(function(resolve, reject) {
-          if (typeof body.email !== 'string'  && typeof body.password !== 'string') {
-            reject();
-          }
-
-          user.findOne({
-            where: {
-              email: body.email
+    }, {
+        hooks: {
+            beforeValidate: function(user, options) {
+                if (typeof user.email === 'string') {
+                    user.email = user.email.toLowerCase().trim();
+                }
             }
-          }).then(function (user) {
-            if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) { //get is a part of sequelize and it returns a value when passed a key (it takes a string) - bcrypt.compareSync compared the password you are passing in the the password of the email that was passed in that is already in the database and is salted and hashed
-                return reject();
-            }
-            resolve(user);
-          }, function (e) {
-            reject();
-          });
-        });
-      }
-    },
-    instanceMethods: { //hides the password and other sensitive database columns
-      toPublicJSON: function () {
-        var json = this.toJSON();
-        return _.pick(json, "id","email","updatedAt","createdAt");
-      }
-    }
-  });
+        },
+        classMethods: {
+            authenticate: function(body) {
+                return new Promise(function(resolve, reject) {
+                    if (typeof body.email !== 'string' && typeof body.password !== 'string') {
+                        reject();
+                    }
 
-  return user;
+                    user.findOne({
+                        where: {
+                            email: body.email
+                        }
+                    }).then(function(user) {
+                        if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) { //get is a part of sequelize and it returns a value when passed a key (it takes a string) - bcrypt.compareSync compared the password you are passing in the the password of the email that was passed in that is already in the database and is salted and hashed
+                            return reject();
+                        }
+                        resolve(user);
+                    }, function(e) {
+                        reject();
+                    });
+                });
+            }
+        },
+        instanceMethods: { //hides the password and other sensitive database columns
+            toPublicJSON: function() {
+                var json = this.toJSON();
+                return _.pick(json, "id", "email", "updatedAt", "createdAt");
+            },
+            generateToken: function(type) {
+                if (!_.isString(type)){
+                    return undefined;
+                }
+
+                try {                    
+                    var stringData = JSON.stringify({id: this.get('id'), type: type});
+                    var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
+                    var token = jwt.sign({
+                        token: encryptedData
+                    }, 'qwerty098'); //sign takes an object.token and a string password
+
+                    return token;
+                } catch (e) {
+                    console.error(e);
+                    return 'my_string';
+                }
+            }
+        }
+    });
+
+    return user;
 };
--- server.js
+++ server.js
@@ -15,161 +15,173 @@ app.use(bodyParser.json()); //takes data posted and makes it JSON
 
 //GET /todos get request http /todos
 app.get('/todos', function(req, res) {
-  var query = req.query;
-  var nowBoolean = ('true' == query.completed); //this converts the string 'true' to true and false to 'false'
+    var query = req.query;
+    var nowBoolean = ('true' == query.completed); //this converts the string 'true' to true and false to 'false'
 
-  var where = {};
+    var where = {};
 
-  // if the query string contains completed get the string value 'true' or 'false' and convert it to a boolean value
-  // add this completed: boolean value to the where object
-  if (query.hasOwnProperty('completed')) {
-    where.completed = nowBoolean;
-  }
+    // if the query string contains completed get the string value 'true' or 'false' and convert it to a boolean value
+    // add this completed: boolean value to the where object
+    if (query.hasOwnProperty('completed')) {
+        where.completed = nowBoolean;
+    }
 
-  // If there is a q query string and it contains something then add the query string value to the where.description object key
-  if (query.hasOwnProperty('q') && query.q.length > 0) {
-    where.description = {
-      $like: '%' + query.q + '%' //$iLike is an option that works with postgres only but is case insensitive, $like is case sensitive in PG
-    };
-  }
+    // If there is a q query string and it contains something then add the query string value to the where.description object key
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.description = {
+            $like: '%' + query.q + '%' //$iLike is an option that works with postgres only but is case insensitive, $like is case sensitive in PG
+        };
+    }
 
-  //  Use the where functionality of sequelize to search db.todo using the where object query parameters
-  db.todo.findAll({
-    where: where
-  }).then(function(todos) {
-    res.json(todos);
-  }, function(e) {
-    res.status(500).send();
-  });
+    //  Use the where functionality of sequelize to search db.todo using the where object query parameters
+    db.todo.findAll({
+        where: where
+    }).then(function(todos) {
+        res.json(todos);
+    }, function(e) {
+        res.status(500).send();
+    });
 
 });
 
 // Get all todos that match the given id parameter
 //GET /todos/:id
 app.get('/todos/:id', function(req, res) {
-  var todoId = Number(req.params.id);
+    var todoId = Number(req.params.id);
 
-  db.todo.findById(todoId).then(function(todo) {
-    if (todo) {
-      res.status(200).json(todo);
-    } else {
-      res.status(404).json({
-        'Message': 'Todo not found'
-      });
-    }
-  }, function(error) {
-    res.status(500).json(error);
-  });
+    db.todo.findById(todoId).then(function(todo) {
+        if (todo) {
+            res.status(200).json(todo);
+        } else {
+            res.status(404).json({
+                'Message': 'Todo not found'
+            });
+        }
+    }, function(error) {
+        res.status(500).json(error);
+    });
 });
 
 app.get('/', function(req, res) {
-  res.send('Todo API Root');
+    res.send('Todo API Root');
 });
 
 //POST REQUEST /todos - can take data
 // needs body-parseer module to send JSON data with post
 
 app.post('/todos', function(req, res) {
-  // Gets rid of any other object keys that might get hacked in by hackers
-  var body = _.pick(req.body, 'description', 'completed');
+    // Gets rid of any other object keys that might get hacked in by hackers
+    var body = _.pick(req.body, 'description', 'completed');
 
-  // Takes the databse version of todo and creates a POST on that
-  db.todo.create(body).then(function(todo) {
-    if (todo) {
-      if (todo.description.trim().length === 0) {
-        return res.status(400).send();
-      } else {
-        res.json(todo.toJSON());
-      }
-    }
-  }).catch(function(error) {
-    res.status(400).json(e);
-  });
+    // Takes the databse version of todo and creates a POST on that
+    db.todo.create(body).then(function(todo) {
+        if (todo) {
+            if (todo.description.trim().length === 0) {
+                return res.status(400).send();
+            } else {
+                res.json(todo.toJSON());
+            }
+        }
+    }).catch(function(error) {
+        res.status(400).json(e);
+    });
 
-});
-
-app.post('/users', function(req, res) {
-  var body = _.pick(req.body, 'email', 'password');
-
-  db.user.create(body).then(function(user) {
-    res.json(user.toPublicJSON());
-  }, function(e) {
-    res.status(400).json(e);
-  });
-});
-
-//POST /user/login
-
-app.post('/users/login', function(req, res) {
-  var body = _.pick(req.body, "email", "password");
-
-  db.user.authenticate(body).then(function(user) {
-    res.json(user.toPublicJSON());
-  }, function(e) {
-    res.status(401).send();
-  });
 });
 
 // DELETE todos/:id
 
 app.delete('/todos/:id', function(req, res) {
-  // Get the passed in id from the user to be removed
-  var todoId = Number(req.params.id);
+    // Get the passed in id from the user to be removed
+    var todoId = Number(req.params.id);
 
-  db.todo.destroy({
-    where: {
-      id: todoId
-    }
-  }).then(function(itemsToDelete) {
-    if (itemsToDelete) {
-      res.status(200).json('Your (' + itemsToDelete + ') todo with the id of ' + todoId + ' has been deleted.');
-    } else {
-      res.status(404).json({
-        "error": "Hold on partner, that id does not exist!"
-      });
-    }
-  }, function(e) {
-    // Sends a server error 500 in case there is trouble connecting to server (rather than crashing).
-    res.status(500).send();
-  });
+    db.todo.destroy({
+        where: {
+            id: todoId
+        }
+    }).then(function(itemsToDelete) {
+        if (itemsToDelete) {
+            res.status(200).json('Your (' + itemsToDelete + ') todo with the id of ' + todoId + ' has been deleted.');
+        } else {
+            res.status(404).json({
+                "error": "Hold on partner, that id does not exist!"
+            });
+        }
+    }, function(e) {
+        // Sends a server error 500 in case there is trouble connecting to server (rather than crashing).
+        res.status(500).send();
+    });
 });
 
 // UPDATE Todos by ID Using Sequelize
 
 app.put('/todos/:id', function(req, res) {
-  var todoId = Number(req.params.id);
-  var body = _.pick(req.body, "description", "completed");
-  var attributes = {};
+    var todoId = Number(req.params.id);
+    var body = _.pick(req.body, "description", "completed");
+    var attributes = {};
 
-  if (body.hasOwnProperty('completed')) {
-    attributes.completed = body.completed;
-  }
-
-  if (body.hasOwnProperty('description')) {
-    attributes.description = body.description;
-  }
-
-  db.todo.findById(todoId).then(function(todo) {
-    if (todo) {
-      todo.update(attributes).then(function(todo) {
-        res.json(todo);
-      }, function(e) {
-        res.status(400).json(e);
-      });
-    } else {
-      res.status(404).send();
+    if (body.hasOwnProperty('completed')) {
+        attributes.completed = body.completed;
     }
-  }, function(e) {
-    res.status(500).send();
-  });
+
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
+    }
+
+    db.todo.findById(todoId).then(function(todo) {
+        if (todo) {
+            todo.update(attributes).then(function(todo) {
+                res.json(todo);
+            }, function(e) {
+                res.status(400).json(e);
+            });
+        } else {
+            res.status(404).send();
+        }
+    }, function(e) {
+        res.status(500).send();
+    });
 });
 
 app.get('/', function(req, res) {
-  res.send('Todo API Root');
+    res.send('Todo API Root');
 });
 
-db.sequelize.sync( {force: true}).then(function() {
-  app.listen(PORT, function() {
-    console.log('Express listening on port' + PORT + '!');
-  });
+/////////////// USERS SECTION //////////////
+
+app.post('/users', function(req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.create(body).then(function(user) {
+        res.json(user.toPublicJSON());
+    }, function(e) {
+        res.status(400).json(e);
+    });
+});
+
+//POST /user/login
+
+app.post('/users/login', function(req, res) {
+    var body = _.pick(req.body, "email", "password");
+
+    db.user.authenticate(body).then(function(user) {
+        var token = user.generateToken('authentication');
+        if (token) { //if token generation went well do beow if not send an error
+            res.header('Auth', token).json(user.toPublicJSON());
+        } else {
+            res.status(401).send();
+        }
+
+    }, function(e) {
+        res.status(401).send();
+    });
+});
+
+// Setup Sequelize
+
+db.sequelize.sync({
+    force: true
+}).then(function() {
+    app.listen(PORT, function() {
+        console.log('Express listening on port' + PORT + '!');
+    });
 });
